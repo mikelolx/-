82e312578ac589e0a3b2f70d8afbe8507d3412a9
mikelolx <47793088+mikelolx@users.noreply.github.com>
2019-02-22 07:48:45 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3440,4 +3440,14 @@ client.on('message', message => {
     }
 });
 
+client.on('typingStart', (ch, user) => {
+    if(user.presence.status === 'offline') {
+        
+        ch.send(`${user} هاهاهاا , كشفتك وانت تكتب ي اوف لاين`)
+        .then(msg => {
+            msg.delete(10000)
+        })
+    }
+});
+
 client.login(process.env.BOT_TOKEN)
