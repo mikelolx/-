3ab7541188d13bb300cb3ccd4cd29efbaf447708
mikelolx <47793088+mikelolx@users.noreply.github.com>
2019-02-24 06:19:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4254,4 +4254,491 @@ member.addRole( n3k4a[member.user.id].roles.shift());
 }
 });
 
+var guilds = {};
+client.on('message',async message => {
+ var prefix = '-';//البرفكس
+  if(message.content.startsWith(prefix + "buy")) {
+ 
+if(!message.channel.guild) return message.reply(' ');
+ 
+ 
+  let submite = message.guild.channels.find(`name`, "اسم روم الي توصل اليه ليبون يشترون رتبة");
+ 
+  if(!submite) return message.channel.send("لايوجد روم خاص للي يوصل الشراء اليه :x:");
+  let filter = m => m.author.id === message.author.id;
+ 
+    let thisMessage;
+ 
+    let thisFalse;
+ 
+    message.channel.send('**ما تشتري ؟**').then(msg => {
+ 
+ 
+ 
+    message.channel.awaitMessages(filter, {
+ 
+      max: 1,
+ 
+      time: 90000,
+ 
+      errors: ['time']
+ 
+    })
+ 
+    .then(collected => {
+ 
+      collected.first().delete();
+ 
+      thisMessage = collected.first().content;
+ 
+      let boi;
+ 
+      msg.edit('**الرجاء كتابة اسمك**').then(msg => {
+ 
+ 
+ 
+          message.channel.awaitMessages(filter, {
+ 
+            max: 1,
+ 
+            time: 90000,
+ 
+            errors: ['time']
+ 
+          })
+ 
+          .then(collected => {
+ 
+            collected.first().delete();
+ 
+            boi = collected.first().content;
+ 
+            let boi2;
+ 
+            msg.edit('**الرجاء تحويل المبلغ المطلوب للشخص المطلوب|يوجد لديك 9 ثواني**').then(msg => {
+ 
+ 
+ 
+              message.channel.awaitMessages(filter, {
+ 
+                max: 1,
+ 
+                time: 90000,
+ 
+                errors: ['time']
+ 
+              })
+ 
+              .then(collected => {
+ 
+                collected.first().delete();
+ 
+              boi2 = collected.first().content;
+ 
+      msg.edit('**هل متاكد من شرائك الرتبة اخي الكريم الرجاء الاجابة بنعم ام لا**');
+ 
+ message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+ 
+        max: 1,
+ 
+        time: 90000,
+ 
+        errors: ['time']
+ 
+      })
+ 
+      .then(collected => {
+ 
+        if(collected.first().content === 'لا') {
+ 
+          msg.delete();
+ 
+          message.delete();
+ 
+          thisFalse = false;
+ 
+        }
+ 
+        if(collected.first().content === 'نعم') {
+ 
+          if(thisFalse === false) return;
+ 
+          msg.edit('**Done ✅, تم بنجاح شراء الرتبة الرجاء الانتظار حين الادارة ترد عليك**');
+ 
+          collected.first().delete();
+ 
+          submite.send(`@everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**[ ${message.guild.name}:arrow_down: ] Submit**
+ 
+[**هل يريد شراء الرتبة او كهدية**]:
+${thisMessage}
+ 
+[**اسم الذي يريد شراء الرتبة**]:
+${boi}
+ 
+[**هل تم التحويل ام لا**]:
+${boi2}
+ 
+[**اسم الشاري بمنشنة**]:
+${message.author}
+ 
+[**ايد�� الشاري**]:
+${message.author.id}`);//Fras#2729
+ 
+        }
+ 
+      }
+ 
+  );
+ 
+});
+ 
+    });
+ 
+  }
+ 
+    );
+ 
+  });
+ 
+}
+ 
+);
+ 
+    })}});
+
+let anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8"));
+let con = JSON.parse(fs.readFileSync("./config.json", "UTF8"));
+client.on("message", message => {
+    if(!message.channel.guild) return;
+    let user = anti[message.guild.id+message.author.id]
+    let num = message.content.split(" ").slice(1).join(" ");
+    if(!anti[message.guild.id+message.author.id]) anti[message.guild.id+message.author.id] = {
+        actions: 0
+    }
+    if(!con[message.guild.id]) con[message.guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3,
+        time: 30
+    }
+if(message.content.startsWith(prefix+"limits")) {
+ 
+ 
+if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return;
+    if(message.content.startsWith(prefix + "limitsban")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].banLimit = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].banLimit} **`)
+    }
+    if(message.content.startsWith(prefix + "limitskick")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].kickLimits = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].kickLimits}**`)
+    }
+    if(message.content.startsWith(prefix + "limitsroleD")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].roleDelLimit = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].roleDelLimit}**`)
+    }
+    if(message.content.startsWith(prefix + "limitsroleC")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].roleCrLimits = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].roleCrLimits}**`)
+    }
+    if(message.content.startsWith(prefix + "limitschannelD")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].chaDelLimit = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].chaDelLimit}**`)
+    }
+    if(message.content.startsWith(prefix + "limitstime")) {
+        if(!num) return message.channel.send("**⇏ | أرسل رقم ! **");
+        if(isNaN(num)) return message.channel.send("**⇏ | أرقام فقط ! **");
+        con[message.guild.id].time = num;
+        message.channel.send(`**⇏ | تم التغيير اِلي : ${con[message.guild.id].time}**`)
+    }
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+}
+});
+client.on("channelDelete", async channel => {
+    const entry1 = await channel.guild.fetchAuditLogs({type: 'CHANNEL_DELETE'}).then(audit => audit.entries.first())
+    console.log(entry1.executor.username)
+    const entry = entry1.executor
+    if(!con[channel.guild.id]) con[channel.guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3
+    }
+    if(!anti[channel.guild.id+entry.id]) {
+    anti[channel.guild.id+entry.id] = {
+        actions: 1
+        }
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+    } else {
+        anti[channel.guild.id+entry.id].actions = Math.floor(anti[channel.guild.id+entry.id].actions+1)
+        console.log("TETS");
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+        if(anti[channel.guild.id+entry.id].actions >= con[channel.guild.id].chaDelLimit) {
+            channel.guild.members.get(entry.id).ban().catch(e => channel.guild.owner.send(`**⇏ | ${entry.username} قام بمسح الكثير من الرومات **`))
+            anti[channel.guild.id+entry.id].actions = "0"
+            fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+    if(e) throw e;
+});
+fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if(e) throw e;
+});
+        }
+    }
+   
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+});
+ 
+client.on("roleDelete", async channel => {
+    const entry1 = await channel.guild.fetchAuditLogs({type: 'ROLE_DELETE'}).then(audit => audit.entries.first())
+    console.log(entry1.executor.username)
+    const entry = entry1.executor
+    if(!con[channel.guild.id]) con[channel.guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3
+    }
+    if(!anti[channel.guild.id+entry.id]) {
+    anti[channel.guild.id+entry.id] = {
+        actions: 1
+        }
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+    } else {
+        anti[channel.guild.id+entry.id].actions = Math.floor(anti[channel.guild.id+entry.id].actions+1)
+        console.log("TETS");
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+        if(anti[channel.guild.id+entry.id].actions >= con[channel.guild.id].roleDelLimit) {
+            channel.guild.members.get(entry.id).ban().catch(e => channel.guild.owner.send(`**⇏ | ${entry.username} قام بمسح الكثير من الرتب **`))
+            anti[channel.guild.id+entry.id].actions = "0"
+            fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+    if(e) throw e;
+});
+fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if(e) throw e;
+});
+        }
+    }
+   
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+});
+ 
+client.on("roleCreate", async channel => {
+    const entry1 = await channel.guild.fetchAuditLogs({type: 'ROLE_CREATE'}).then(audit => audit.entries.first())
+    console.log(entry1.executor.username)
+    const entry = entry1.executor
+    if(!con[channel.guild.id]) con[channel.guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3
+    }
+    if(!anti[channel.guild.id+entry.id]) {
+    anti[channel.guild.id+entry.id] = {
+        actions: 1
+        }
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+    } else {
+        anti[channel.guild.id+entry.id].actions = Math.floor(anti[channel.guild.id+entry.id].actions+1)
+        console.log("TETS");
+        setTimeout(() => {
+            anti[channel.guild.id+entry.id].actions="0"
+        }, con[channel.guild.id].time*1000)
+        if(anti[channel.guild.id+entry.id].actions >= con[channel.guild.id].roleCrLimits) {
+            channel.guild.members.get(entry.id).ban().catch(e => channel.guild.owner.send(`**⇏ | ${entry.username} قام بأنشاء الكثير من الرتب **`))
+            anti[channel.guild.id+entry.id].actions = "0"
+            fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+    if(e) throw e;
+});
+fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if(e) throw e;
+});
+        }
+    }
+   
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+});
+ 
+client.on("guildBanAdd", async (guild, user) => {
+    const entry1 = await channel.fetchAuditLogs({type: 'MEMBER_BAN_ADD'}).then(audit => audit.entries.first())
+    console.log(entry1.executor.username)
+    const entry = entry1.executor
+    if(!con[guild.id]) con[guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3
+    }
+    if(!anti[guild.id+entry.id]) {
+    anti[guild.id+entry.id] = {
+        actions: 1
+        }
+        setTimeout(() => {
+            anti[guild.id+entry.id].actions="0"
+        }, con[guild.id].time*1000)
+    } else {
+        anti[guild.id+entry.id].actions = Math.floor(anti[guild.id+entry.id].actions+1)
+        console.log("TETS");
+        setTimeout(() => {
+            anti[guild.id+entry.id].actions="0"
+        }, con[guild.id].time*1000)
+        if(anti[guild.id+entry.id].actions >= con[guild.id].banLimit) {
+            channel.members.get(entry.id).ban().catch(e => channel.owner.send(`**⇏ | ${entry.username} حاول حظر جميع الأعضاء **`))
+            anti[guild.id+entry.id].actions = "0"
+            fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+    if(e) throw e;
+});
+fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if(e) throw e;
+});
+        }
+    }
+   
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+});
+ 
+client.on("guildKickAdd", async (guild, user) => {
+    const entry1 = await channel.fetchAuditLogs({type: 'MEMBER_KICK'}).then(audit => audit.entries.first())
+    console.log(entry1.executor.username)
+    const entry = entry1.executor
+    if(!con[guild.id]) con[guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3
+    }
+    if(!anti[guild.id+entry.id]) {
+    anti[guild.id+entry.id] = {
+        actions: 1
+        }
+        setTimeout(() => {
+            anti[guild.id+entry.id].actions="0"
+        }, con[guild.id].time*1000)
+    } else {
+        anti[guild.id+entry.id].actions = Math.floor(anti[guild.id+entry.id].actions+1)
+        console.log("TETS");
+        setTimeout(() => {
+            anti[guild.id+entry.id].actions="0"
+        }, con[guild.id].time*1000)
+        if(anti[guild.id+entry.id].actions >= con[guild.id].banLimit) {
+            channel.members.get(entry.id).ban().catch(e => channel.owner.send(`**⇏ | ${entry.username} حاول حظر جميع الأعضاء **`))
+            anti[guild.id+entry.id].actions = "0"
+            fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+    if(e) throw e;
+});
+fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+    if(e) throw e;
+});
+        }
+    }
+   
+    fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+});
+ 
+client.on("guildMemberRemove", async member => {
+    const entry1 = await member.guild.fetchAuditLogs().then(audit => audit.entries.first())
+    if(entry1.action === "MEMBER_KICK") {
+        const entry2 = await member.guild.fetchAuditLogs({type: "MEMBER_KICK"}).then(audit => audit.entries.first())
+        const entry = entry2.executor;
+        if(!con[member.guild.id]) con[guild.id] = {
+            banLimit: 3,
+            chaDelLimit: 3,
+            roleDelLimit: 3,
+            kickLimits: 3,
+            roleCrLimits: 3
+        }
+        if(!anti[member.guild.id+entry.id]) {
+        anti[member.guild.id+entry.id] = {
+            actions: 1
+            }
+            setTimeout(() => {
+                anti[member.guild.id+entry.id].actions="0"
+            }, con[member.guild.id].time*1000)
+        } else {
+            anti[member.guild.id+entry.id].actions = Math.floor(anti[member.guild.id+entry.id].actions+1)
+            console.log("TETS");
+            setTimeout(() => {
+                anti[member.guild.id+entry.id].actions="0"
+            }, con[member.guild.id].time*1000)
+            if(anti[member.guild.id+entry.id].actions >= con[member.guild.id].kickLimits) {
+                member.members.get(entry.id).ban().catch(e => member.owner.send(`**⇏ | ${entry.username} حاول حظر جميع الأعضاء **`))
+                anti[member.guild.id+entry.id].actions = "0"
+                fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+        if(e) throw e;
+    });
+    fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+        if(e) throw e;
+    });
+            }
+        }
+       
+        fs.writeFile("./config.json", JSON.stringify(con, null, 2), function(e) {
+            if(e) throw e;
+        });
+        fs.writeFile("./antigreff.json", JSON.stringify(anti, null, 2), function(e) {
+            if(e) throw e;
+        });
+    }
+   
+ 
+});
+
 client.login(process.env.BOT_TOKEN)
